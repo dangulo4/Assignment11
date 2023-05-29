46e352e00748d3c13501f55dc7cc8d8b3ba5461a
Daniel Angulo <dangulo4@gmail.com>
2023-05-29 11:24:48 -0600

    created set speed functions to update speed of slideshow

--- slideshow/js/slideshow.js
+++ slideshow/js/slideshow.js
@@ -1,88 +1,110 @@
 // REWRITTEN TO TAKE ADVANTAGE OF CLOSURES
-const $ = (id) => document.getElementById(id)
+const $ = (id) => document.getElementById(id);
 
 const createSlideshow = function () {
-    // PRIVATE VARIABLES AND FUNCTIONS
-    let timer
-    let play = true    
-    let nodes = { image: null, caption: null }
-    let img = { cache: [], counter: 0 }
-    
-    const stopSlideShow = function () {
-        clearInterval(timer)
-    }
+  // PRIVATE VARIABLES AND FUNCTIONS
+  let timer;
+  let speed = 2000;
+  let play = true;
+  let nodes = { image: null, caption: null };
+  let img = { cache: [], counter: 0 };
 
-    const displayNextImage = function () {
-        if (img.counter === img.cache.length) {
-            img.counter = 0
-        } else {
-            img.counter ++
-        }
-        let image = img.cache[img.counter]
-        nodes.image.src = image.src
-        nodes.caption.innerHTML = image.title
-    }
+  const stopSlideShow = function () {
+    clearInterval(timer);
+  };
 
-    const setPlayText = function (btn) {
+  const displayNextImage = function () {
+    if (img.counter === img.cache.length) {
+      img.counter = 0;
+    } else {
+      img.counter++;
+    }
+    let image = img.cache[img.counter];
+    nodes.image.src = image.src;
+    nodes.caption.innerHTML = image.title;
+  };
+
+  const setPlayText = function (btn) {
+    if (play) {
+      btn.value = 'Resume';
+    } else {
+      btn.value = 'Pause';
+    }
+  };
+
+  // PUBLIC METHODS THAT HAVE ACCESS TO PRIVATE VARIABLES AND FUNCTIONS
+  return {
+    loadImages: function (slides) {
+      for (let i = 0; i < slides.length; i++) {
+        let image = new Image();
+        image.src = slides[i].href;
+        image.title = slides[i].title;
+        img.cache.push(image);
+      }
+      return this;
+    },
+    startSlideShow: function () {
+      if (arguments.length === 2) {
+        nodes.image = arguments[0];
+        nodes.caption = arguments[1];
+      }
+      console.log(speed);
+      document.getElementById('seconds').innerHTML = speed;
+      timer = setInterval(displayNextImage, speed);
+      return this;
+    },
+    createToggleHandler: function () {
+      let me = this;
+      // CLOSURE TO BE USED AS THE CLICK EVENT HANDLER
+      return function () {
+        // 'THIS' IS THE CLICKED BUTTON
+        // 'ME' IS THE OBJECT LITERAL
         if (play) {
-            btn.value = 'Resume'
+          stopSlideShow();
         } else {
-            btn.value = 'Pause'
+          me.startSlideShow();
         }
-    }
+        setPlayText(this);
+        // TOGGLE PLAY 'FLAG'
+        play = !play;
+      };
+    },
+    createGetSpeedHandler: function () {
+      const getSpeed = () => {
+        let setSpeed = parseInt(prompt('Enter speed'));
+        speed = setSpeed;
+        console.log(`The speed is now set to ${speed}`);
+        document.getElementById('seconds').innerHTML = speed;
+      };
+      return getSpeed;
+    },
+    createSetSpeedHandler: function () {
+      const setSpeed = () => {
+        timer = setInterval(displayNextImage, speed);
+      };
+      return setSpeed;
+    },
+  };
+};
 
-    // PUBLIC METHODS THAT HAVE ACCESS TO PRIVATE VARIABLES AND FUNCTIONS
-    return {
-        loadImages: function (slides) {
-            for (let i = 0; i < slides.length; i++) {
-                let image = new Image()
-                image.src = slides[i].href
-                image.title = slides[i].title
-                img.cache.push(image)
-            }
-            return this
-        },
-        startSlideShow: function () {
-            if (arguments.length === 2) {
-                nodes.image = arguments[0]
-                nodes.caption = arguments[1]
-            }
-            timer = setInterval(displayNextImage, 2000)
-            return this
-        },
-        createToggleHandler: function () {
-            let me = this
-            // CLOSURE TO BE USED AS THE CLICK EVENT HANDLER
-            return function () {
-                // 'THIS' IS THE CLICKED BUTTON
-                // 'ME' IS THE OBJECT LITERAL
-                if (play) {
-                    stopSlideShow();
-                } else {
-                    me.startSlideShow();
-                }
-                setPlayText(this)
-                // TOGGLE PLAY 'FLAG'
-                play = !play
-            }
-        }
-    }
-}
+// const setSpeed = createSetSpeedHandler();
+// setSpeed();
 
 // CREATE THE SLIDESHOW OBJECT
-const slideshow = createSlideshow()
+const slideshow = createSlideshow();
 
 window.addEventListener('load', () => {
-    const slides = [
-        {href: 'images/backpack.jpg', title: 'He backpacks in the Sierras often'},
-        {href: 'images/boat.jpg', title: 'He loves his boat'},
-        {href: 'images/camaro.jpg', title: 'He loves his Camaro more'},
-        {href: 'images/punk.jpg', title: 'He used to be in a punk band and toured with No Doubt and Sublime'},
-        {href: 'images/race.jpg', title: 'He\'s active and loves obstacle coarse racing'},
-        {href: 'images/wakesurf.jpg', title: 'He loves to wakesurf behind his boat'}
-    ]
-	// START THE SLIDESHOW
-    slideshow.loadImages(slides).startSlideShow($('image'), $('caption'))
-    // PAUSE THE SLIDESHOW
-    $('play_pause').onclick = slideshow.createToggleHandler()
-})
+  const slides = [
+    { href: 'images/backpack.jpg', title: 'He backpacks in the Sierras often' },
+    { href: 'images/boat.jpg', title: 'He loves his boat' },
+    { href: 'images/camaro.jpg', title: 'He loves his Camaro more' },
+    { href: 'images/punk.jpg', title: 'He used to be in a punk band and toured with No Doubt and Sublime' },
+    { href: 'images/race.jpg', title: "He's active and loves obstacle coarse racing" },
+    { href: 'images/wakesurf.jpg', title: 'He loves to wakesurf behind his boat' },
+  ];
+  // START THE SLIDESHOW
+  slideshow.loadImages(slides).startSlideShow($('image'), $('caption'));
+  // PAUSE THE SLIDESHOW
+  $('play_pause').onclick = slideshow.createToggleHandler();
+  $('speed').onclick = slideshow.createGetSpeedHandler();
+});
